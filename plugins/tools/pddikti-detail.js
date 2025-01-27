62cbfdb80f22f0b142fcd862e0948b0424c46991
prasbhara0604 <prasbhara0604@gmail.com>
2025-01-27 06:30:19 +0000

    omke gams

--- plugins/tools/pddikti-detail.js
+++ plugins/tools/pddikti-detail.js
@@ -1,54 +1,22 @@
-import { pddiktiSearch } from "../../lib/scraper/pddikti.js";
+import { mahasiswaDetail } from "../../lib/scraper/pddikti.js";
 
-export const handler = 'pddikti'
-export const description = 'Resolve Information from Pddikti by NIM/Name'
+export const handler = 'pdd'
+export const description = 'Resolve Information from PDDikti by NIM/Name'
 export default async ({ sock, m, id, psn, sender, noTel, caption, attf }) => {
-    const result = await pddiktiSearch(psn)
-    const rows = [
-        {
-            header: 'all feature',
-            highlight_label: 'V2',
-            title: '',
-            description: 'Display to menu',
-            id: 'menu',
-        },
-    ]
-    result.forEach((d, i) => {
-        rows.push({
-            header: d.link,
-            highlight_label: d.nim,
-            title: d.nama,
-            description: `${d.programStudi} - ${d.perguruanTinggi}`,
-            id: `pdd ${d.link}`,
-        })
-    })
+    if (psn == "") return await sock.sendMessage(id, { text: 'Masukkan NIM/Nama untuk dicari,contoh `pddikti SSI202203088`' })
+    await sock.sendMessage(id, { text: 'Tunggu sebentar,ini sedikit memakan waktu ...`' })
+    
+    const result = await mahasiswaDetail(psn)
+    let text = `\`[PDDIKTI DATA RESULT]\``;
+    text += `Nama : \`${result['Nama']}\``;
+    text += `PT : \`${result['Perguruan Tinggi']}\``;
+    text += `JK : \`${result['Jenis Kelamin']}\``;
+    text += `Tgl. Msk : \`${result['Tanggal Masuk']}\``;
+    text += `NIM : \`${result['NIM']}\``;
+    text += `Prodi : \`${result['Jenjang - Program Studi']}\``;
+    text += `Status awal Mhs : \`${result['Status Awal Mahasiswa']}\``;
+    text += `Status akhir Mhs : \`${result['Status Terakhir Mahasiswa']}\``;
     sock.sendMessage(id, {
-        text: `Hasil Pencarian *PDDikti* untuk ${psn}`,
-        footer: 'Kanata-V2',
-        buttons: [
-
-            {
-                buttonId: 'action',
-                buttonText: {
-                    displayText: 'PDDikti Scraper by Roy'
-                },
-                type: 4,
-                nativeFlowInfo: {
-                    name: 'single_select',
-                    paramsJson: JSON.stringify({
-                        title: 'message',
-                        sections: [
-                            {
-                                title: 'Hasil Pencarian Pddikti',
-                                highlight_label: '!',
-                                rows
-                            },
-                        ],
-                    }),
-                },
-            },
-        ],
-        headerType: 1,
-        viewOnce: true
+        text
     }, { quoted: m });
 };
--- plugins/tools/pddikti.js
+++ plugins/tools/pddikti.js
@@ -1,5 +1,48 @@
-export const handler='pdd'
-export const description=''
-export default async ({ sock, m, id, psn, sender, noTel, caption,attf }) => {
-    
+import { pddiktiSearch } from "../../lib/scraper/pddikti.js";
+
+export const handler = 'pddikti'
+export const description = 'Resolve Information from Pddikti by NIM/Name'
+export default async ({ sock, m, id, psn, sender, noTel, caption, attf }) => {
+    if (psn == "") return await sock.sendMessage(id, { text: 'Masukkan Nim/Nama untuk dicari,contoh `pddikti SSI202203088`' })
+    await sock.sendMessage(id, { text: 'Tunggu sebentar,ini sedikit memakan waktu ...`' })
+    const result = await pddiktiSearch(psn)
+    const rows = []
+    result.forEach((d, i) => {
+        rows.push({
+            header: d.link,
+            highlight_label: d.nim,
+            title: d.nama,
+            description: `${d.programStudi} - ${d.perguruanTinggi}`,
+            id: `pdd ${d.link}`,
+        })
+    })
+    await sock.sendMessage(id, {
+        text: `Hasil Pencarian *PDDikti* untuk ${psn}`,
+        footer: 'Kanata-V2',
+        buttons: [
+
+            {
+                buttonId: 'action',
+                buttonText: {
+                    displayText: 'PDDikti Scraper by Roy'
+                },
+                type: 4,
+                nativeFlowInfo: {
+                    name: 'single_select',
+                    paramsJson: JSON.stringify({
+                        title: 'message',
+                        sections: [
+                            {
+                                title: 'Hasil Pencarian Pddikti',
+                                highlight_label: '!',
+                                rows
+                            },
+                        ],
+                    }),
+                },
+            },
+        ],
+        headerType: 1,
+        viewOnce: true
+    }, { quoted: m });
 };
