47de2cad87a39730f255dd080f3ddc58fe3e420d
prasbhara0604 <prasbhara0604@gmail.com>
2025-01-28 10:30:07 +0000

    add retro

--- plugins/image-creative/retro.js
+++ plugins/image-creative/retro.js
@@ -8,13 +8,14 @@ export default async ({ sock, m, id, psn, sender, noTel, caption, attf }) => {
         try {
             // Mengunggah gambar dan mengubah menjadi HD menggunakan API retro
             const imageUrl = await uploadGambar2(attf);
-            const { url } = await fetch(`'https://fastrestapis.fasturl.cloud/aiimage/imgcolorize?url=${imageUrl}`);
+            const { url } = await fetch(`https://fastrestapis.fasturl.cloud/aiimage/imgcolorize?url=${imageUrl}`);
             await sock.sendMessage(id, {
                 image: { url },
                 caption: '📷 Retro Image berhasil 🎉'
             }, { quoted: m });
 
         } catch (error) {
+            // Penanganan kesalahan dengan pesan lebih informatif
             await sock.sendMessage(id, { text: `⚠️ Terjadi kesalahan saat memproses gambar. Coba lagi nanti ya!\n\nError: ${error.message}` });
         }
         return;
