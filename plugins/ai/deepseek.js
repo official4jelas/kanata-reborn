47f221b9523005c2ea599a5c1fdf020b101cea07
idlanyor <prasbhara0604@gmail.com>
2025-02-01 12:39:25 +0700

    change to ryzen

--- plugins/ai/deepseek.js
+++ plugins/ai/deepseek.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const handler = 'deepseek'
-export const description = "Deepseek AI Provided by Hikaru FastUrl";
+export const description = "Deepseek AI Provided by Ryzendesu";
 export default async ({ sock, m, id, psn, sender, noTel, caption }) => {
     if (psn === '') {
         sock.sendMessage(id, {
@@ -12,53 +12,22 @@ export default async ({ sock, m, id, psn, sender, noTel, caption }) => {
     }
     sock.sendMessage(id, { react: { text: '⏱️', key: m.key } })
     try {
-        const payload = {
-            "model": "deepseek-ai/DeepSeek-R1",
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "Be a helpful assistant"
-                },
-                {
-                    "role": "user",
-                    "content": psn
-                }
-            ],
-            "max_tokens": 1000,
-            "temperature": 0.2,
-            "presence_penalty": 0,
-            "frequency_penalty": 0,
-            "top_p": 1,
-            "top_k": 50
-        };
 
-        const cfg = {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(payload)
-        };
+
+
 
         try {
-            const response = await fetch("https://fastrestapis.fasturl.cloud/aillm/deepseek", cfg);
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
+            const prompt = "be a helpfull assistant"
+            const response = await fetch(`https://api.ryzendesu.vip/api/ai/deepseek?text=${psn}&prompt=${prompt}`);
 
             const data = await response.json();
 
-            let text = '';
-            text += 'Model: ' + data.result.model;
-            text += '\nResult: ' + data.result.choices[0].message.content;
 
-            await sock.sendMessage(id, { text });
+            await sock.sendMessage(id, { text: data.answer });
             await sock.sendMessage(id, { react: { text: '✅', key: m.key } })
         } catch (error) {
             console.error("Error:", error);
-        await sock.sendMessage(id, { react: { text: '❌', key: m.key } })
+            await sock.sendMessage(id, { react: { text: '❌', key: m.key } })
         }
 
     } catch (error) {
