7df4bc11e21e7920830a6aa2a11dafb87e6d3809
prasbhara0604 <prasbhara0604@gmail.com>
2025-02-04 08:08:23 +0000

    update yt-short

--- plugins/downloader/yt-shorts.js
+++ plugins/downloader/yt-shorts.js
@@ -1,5 +1,4 @@
-import { ryzen } from "../../helper/ryzen.js";
-
+import { ytShorts } from "../../lib/scraper/yt-shorts.js";
 export const description = "YouTube Short Downloader provided by *Roidev*";
 export const handler = ['ysd', 'yd2']
 export default async ({ sock, m, id, psn, sender, noTel, caption }) => {
@@ -11,10 +10,8 @@ export default async ({ sock, m, id, psn, sender, noTel, caption }) => {
     }
     try {
         await sock.sendMessage(id, { text: '🔄 *Sedang diproses...* \n_Mohon tunggu sebentar_ ...' });
-        let { data } = await ryzen('downloader/ytmp4', {
-            params: { url: psn }
-        })
-        await sock.sendMessage(id, { video: await fetch(data.url) });
+        let { videoSrc } = await ytShorts()
+        await sock.sendMessage(id, { video: { url: videoSrc } });
     } catch (error) {
         await sock.sendMessage(id, { text: '❌ *Ups,Terjadi kesalahan Silahkan coba beberapa saat lagi*' });
         throw error
